d27a59ea2595035b0dcf377e4a6869a592a86644
jayson7 <lexxiijoo70@gmail.com>
2024-10-29 15:13:14 +0100

    task manager

---
+++ main.py
@@ -0,0 +1,17 @@
+# a basic python list doesnt work a large set of data and takes a lot of data which is why we need numpy 
+list = [1,2,3,4,5,6]
+
+# A Python library for numerical computing, allowing fast array operations.
+import numpy as np 
+
+x = np.array([1, 2, 3])
+
+print (type(x))
+
+print (x)
+
+# anything in a numpy array has to be the same data types 
+
+# ndarray = n-dimensional array 
+
+np1 = np.array([0,1,2,3,4,5])
